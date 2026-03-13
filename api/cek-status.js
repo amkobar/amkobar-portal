142fcdf53fbc71bf1ea543d7818ca23f0ebf8cda
amkobar <me.amkobar@gmail.com>
2026-03-13 23:24:33 +0700

    Update cek-status.js

--- api/cek-status.js
+++ api/cek-status.js
@@ -128,7 +128,7 @@ export default async function handler(req, res) {
 
     return res.status(200).json(result);
 
-  } catch (err) {
-    return res.status(500).json({ error: 'Terjadi kesalahan server.' });
+ } catch (err) {
+    return res.status(500).json({ error: err.message || 'Terjadi kesalahan server.' });
   }
 }

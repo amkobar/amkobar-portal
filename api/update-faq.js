49d1ec7600f6f8f334984e164392d8ee21d84126
amkobar <me.amkobar@gmail.com>
2026-04-22 22:40:04 +0700

    Update update-faq.js

--- api/update-faq.js
+++ api/update-faq.js
@@ -30,7 +30,8 @@ export default async function handler(req, res) {
             'Jawaban': { rich_text: [{ text: { content: jawaban || '' } }] },
             'Kategori': { select: { name: kategori || 'Publik' } },
             'Urutan': { number: urutan || 0 },
-            'Aktif': { checkbox: true }
+            'Aktif': { checkbox: true },
+'Aplikasi': { select: { name: aplikasi || 'Semua' } }
           }
         })
       });
